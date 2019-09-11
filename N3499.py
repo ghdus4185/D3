6eb3b73a2ebf0e3fe515325f7285a65abe114a62
changho <ckdghdus@naver.com>
2019-09-11 15:30:14 +0900

    0911

---
+++ N3499.py
@@ -0,0 +1,7 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    card = input().split()
+    L = card[:N//2]
+    R = card[N//2:]
+    
--- N5603.py
+++ N5603.py
@@ -6,20 +6,7 @@ for tc in range(1, T+1):
     N = int(input())
     a = [int(input()) for _ in range(N)]
     avg = sum(a) // N
-    cnt = 0
-    while 1:
-        if max(a) > avg:
-            x = a[a.index(max(a))]
-            y = a[a.index(min(a))]
-            if x - avg < avg - y:
-                cnt += x - avg
-                a[a.index(max(a))] -= x - avg
-                a[a.index(min(a))] += x - avg
-            else:
-                cnt += avg - y
-                a[a.index(max(a))] -= avg - y
-                a[a.index(min(a))] += avg - y
-
-        if max(a) == avg:
-            break
-    print('#{} {}'.format(tc, cnt))
+    s = 0
+    for i in range(N):
+        s += abs(avg - a[i])
+    print('#{} {}'.format(tc, s//2))
