c0ecb018d077f4bc91bde1d8b5a0605b16766b44
ghdus4185 <ckdghdus@naver.com>
2019-10-18 17:40:35 +0900

    1018

--- N2383.py
+++ N2383.py
@@ -4,7 +4,6 @@ sys.stdin = open('input.txt', 'r')
 def ndpr(n,k):
     global minV
     if n == k:
-        print(dp)
         length = [0] * p
         for i in range(p):
             length[i] = [(abs(people[i][0] - stair[dp[i]-1][0]) + abs(people[i][1] - stair[dp[i]-1][1])), dp[i], stair[dp[i]-1][2]]
@@ -20,7 +19,7 @@ def ndpr(n,k):
         w1, w2 = [0]*3, [0]*3
         c1, c2 = 0, 0
         f = 0
-        while f < 6:
+        while f < p:
             for i in range(3):
                 if w1[i] != 0:
                     w1[i] -= 1
@@ -33,6 +32,7 @@ def ndpr(n,k):
                     if w2[i] == 0:
                         c2 -= 1
                         check[1] = 0
+                        f += 1
 
             for i in range(p): # 사람 수 만큼
                 if length[i][0] >= 2: # 계단까지거리가 2보다 크면
@@ -43,6 +43,7 @@ def ndpr(n,k):
                         wait1.append(length[i][2])
                     else:
                         wait2.append(length[i][2])
+
             if wait1: # 첫번째 계단에 기다리는사람이있으면
                 for i in range(len(wait1)):
                     if check[0] == 0: # 계단 확인
@@ -51,7 +52,7 @@ def ndpr(n,k):
                                 break
                             if w1[i] == 0:
                                 w1[i] = wait1.pop(0)
-                        c1 += 1
+                                c1 += 1
                         if c1 == 3:
                             check[0] = 1
                             break
@@ -67,13 +68,13 @@ def ndpr(n,k):
                                 w2[i] = wait2.pop(0)
                         c2 += 1
                         if c2 == 3:
-                            check[0] = 1
+                            check[1] = 1
                             break
                     else:
                         break
             sec += 1
-        if minV > sec - 1:
-            minV = sec - 1
+        if minV > sec:
+            minV = sec
 
     else:
         for i in range(2):
@@ -99,15 +100,4 @@ for tc in range(1, T+1):
     a = [1, 2]
     dp = [0] * p
     ndpr(0, p)
-    print(minV)
-    # 사람과 모든 계단거리를 비교해서 가장 가까운 거리랑 그 계단번호 저장
-    # for i in range(p):
-    #     min_length = 10000
-    #     for j in range(len(stair)):
-    #         if min_length > abs(people[i][0] - stair[j][0]) + abs(people[i][1] - stair[j][1]):
-    #             min_length = abs(people[i][0] - stair[j][0]) + abs(people[i][1] - stair[j][1])
-    #             idx = stair[j][3] # 계단 번호
-    #     length.append([min_length, idx])
-    # length.sort()
-    # print(length)
-
+    print('#{} {}'.format(tc, minV))
